a3a82e55bc40cd7e8687c89a25f8c857803cecc7
Artur Quaresma <arturquaresma@gmail.com>
2019-12-18 20:42:26 +0000

    Initial commit

---
+++ trofimon/trofimon.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: F44707EC6F3A51D3 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1624
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 425489
+	.include	"typemap.mj.inc"
---
+++ trofimon/trofimon/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using trofimon.Views;
+
+namespace trofimon
+{
+    public partial class App : Application
+    {
+        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
+        //To debug on Android emulators run the web backend against .NET Core not IIS
+        //If using other emulators besides stock Google images you may need to adjust the IP address
+        //public static bool UseMockDataStore = true;
+
+        public App()
+        {
+            InitializeComponent();
+            //MainPage = new MainPage();
+
+            MainPage = new NavigationPage(new Views.Form.Intro());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
