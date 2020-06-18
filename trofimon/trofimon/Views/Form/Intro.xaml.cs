324b98aae206a3a61b2e18afb6d9dbe476ed48fe
Artur Quaresma <artursquaresma@gmail.com>
2020-06-18 14:50:38 +0100

    Intro mInmalsit

--- trofimon/trofimon/Views/Form/Intro.xaml.cs
+++ trofimon/trofimon/Views/Form/Intro.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,10 +12,6 @@ namespace trofimon.Views.Form
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            //https://forums.xamarin.com/discussion/101146/button-that-navigates-to-another-page-can-be-clicked-more-than-once
-            await Navigation.PushAsync(new FormTab());
-        }
+        private async void Button_Clicked(object sender, EventArgs e) => await Navigation.PushAsync(new FormTab());
     }
 }
